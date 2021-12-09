f2259a4f768b221dd30968c7b82ccf6ee52ce98a
nhan1206 <85447657+nhan1206@users.noreply.github.com>
2021-12-09 22:41:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em bé';
+const nameGirl = 'Em ơi';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc mừng em';
+const eventName = 'Ấn vào đi';
 const titleCard = 'Tặng em';
-const contentCard = 'Chúc honey của anh ngày mới tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc e bé của anh ngày mới tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
